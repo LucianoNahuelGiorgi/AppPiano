811a611d1b1ecd2d8b85fe3363240b695b9a6a04
lng <lucianogiorgi@outlook.com.ar>
2023-03-02 11:01:25 -0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano';
+
+  emitirSonido(a: number): void{
+    const audio = new Audio();
+    audio.src = "../assets/sonidos/note" + a + ".wav";
+    audio.load();
+    audio.play();
+  }
+}
